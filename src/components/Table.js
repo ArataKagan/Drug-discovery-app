81052db61a526d8103c80b20cfa7c262b1037532
Arata Kagan <aratakagan@gmail.com>
2021-02-06 14:37:56 -0800

    Add filter function to the table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,4 @@
-import { useTable } from 'react-table';
+import { useTable, useSortBy } from 'react-table';
 
 
 
@@ -9,7 +9,7 @@ function Table({columns, data}) {
         headerGroups,
         rows,
         prepareRow
-    } = useTable({columns, data});
+    } = useTable({columns, data}, useSortBy);
 
     return (
         <table {...getTableProps()}>
@@ -17,7 +17,12 @@ function Table({columns, data}) {
                 {headerGroups.map(headerGroup => (
                     <tr {...headerGroup.getHeaderGroupProps()}>
                         {headerGroup.headers.map(column => (
-                            <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                            <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                                {column.render('Header')}
+                                <span>
+                                    {column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}
+                                </span>
+                            </th>
                         ))}
                     </tr>
                 ))}
