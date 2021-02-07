9d9c9a61cd940c46a5f220e713e0f5dbd926910e
Arata Kagan <aratakagan@gmail.com>
2021-02-06 16:17:55 -0800

    Add pagination

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useTable, useSortBy, useFilters } from 'react-table';
+import { useTable, useSortBy, useFilters, usePagination } from 'react-table';
 
 function TextFilter({
     column: {filterValue, preFilteredRows, setFilter},
@@ -30,46 +30,72 @@ function Table({columns, data}) {
         getTableBodyProps,
         headerGroups,
         rows,
-        prepareRow
+        prepareRow,
+        getRowProps,
+        page,
+        pageOptions,
+        state: {pageIndex, pageSize},
+        previousPage,
+        nextPage,
+        canPreviousPage,
+        canNextPage
     } = useTable(
             {columns, 
              data,
              defaultColumn,
+             initialState: {pageSize: 4}
             }, 
         useFilters,
-        useSortBy
+        useSortBy,
+        usePagination
         );
 
     return (
-        <table {...getTableProps()}>
-            <thead>
-                {headerGroups.map(headerGroup => (
-                    <tr {...headerGroup.getHeaderGroupProps()}>
-                        {headerGroup.headers.map(column => (
-                            <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                                {column.render('Header')}
-                                <div>{column.canFilter ? column.render('Filter') : null}</div>
-                                <span>
-                                    {column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}
-                                </span>
-                            </th>
-                        ))}
-                    </tr>
-                ))}
-            </thead>
-            <tbody {...getTableProps()}>
-                {rows.map((row, i) => {
-                    prepareRow(row)
-                    return (
-                        <tr {...row.getRowProps()}>
-                            {row.cells.map(cell => {
-                                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                            })}
+        <div>
+            <table {...getTableProps()}>
+                <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map(column => (
+                                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                                    {column.render('Header')}
+                                    <div>{column.canFilter ? column.render('Filter') : null}</div>
+                                    <span>
+                                        {column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}
+                                    </span>
+                                </th>
+                            ))}
                         </tr>
-                    )
-                })}
-            </tbody>
-        </table>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {page.map((row, i) => {
+                        prepareRow(row)
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map(cell => {
+                                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                })}
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+            <div>
+                <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                    Previous Page
+                </button>
+                <button onClick={() => nextPage()} disabled={!canNextPage}>
+                    Next Page
+                </button>
+                <div>
+                    Page{' '}
+                    <em>
+                        {pageIndex + 1} of {pageOptions.length}
+                    </em>
+                </div>
+            </div>
+        </div>
     )    
 }
 
