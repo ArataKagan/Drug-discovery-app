d46bfaf0e055a4c74b9280c9d85e26443623502e
Arata Kagan <aratakagan@gmail.com>
2021-02-07 13:23:38 -0800

    Display openFDA data

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import axios from 'axios';
 
 
 function App() {
-  const [drug, setDrug] = useState('');
-  const url = 'https://api.fda.gov/drug/drugsfda.json?limit=5';
+  const [drug, setDrug] = useState([]);
+  const url = 'https://api.fda.gov/drug/drugsfda.json?limit=10';
 
   useEffect(() => {
     getAllData();
@@ -17,95 +17,48 @@ function App() {
     axios.get(url)
     .then((response) => {
       const allData = response.data.results;
+      console.log(allData);
       setDrug(allData);
       console.log(drug);
     })
     .catch(error => console.error(`Error: ${error}`));
   }
 
-  const data = React.useMemo(() =>
-  [
-    {
-        name: 'Kim Parrish',
-        address: '4420 Valley Street, Garnerville, NY 10923',
-        date: '07/11/2020',
-        order: '87349585892118',
-    },
-    {
-        name: 'Michele Castillo',
-        address: '637 Kyle Street, Fullerton, NE 68638',
-        date: '07/11/2020',
-        order: '58418278790810',
-    },
-    {
-        name: 'Eric Ferris',
-        address: '906 Hart Country Lane, Toccoa, GA 30577',
-        date: '07/10/2020',
-        order: '81534454080477',
-    },
-    {
-        name: 'Gloria Noble',
-        address: '2403 Edgewood Avenue, Fresno, CA 93721',
-        date: '07/09/2020',
-        order: '20452221703743',
-    },
-    {
-        name: 'Darren Daniels',
-        address: '882 Hide A Way Road, Anaktuvuk Pass, AK 99721',
-        date: '07/07/2020',
-        order: '22906126785176',
-    },
-    {
-        name: 'Ted McDonald',
-        address: '796 Bryan Avenue, Minneapolis, MN 55406',
-        date: '07/07/2020',
-        order: '87574505851064',
-    },
- ],
- []
-); 
-
-
-
+ 
 const columns = React.useMemo(
   () => [
      {
-        Header: 'User Info',
+        Header: 'FDA Info',
         columns: [
           {
-            Header: 'Name',
-            accessor: 'name',
+            Header: 'Brand',
+            accessor: 'products[0].brand_name',
           },
         {
-            Header: 'Address',
-            accessor: 'address',
+            Header: 'Marketing Status',
+            accessor: 'products[0].marketing_status',
+        },
+        {
+          Header: 'Dosage Form',
+          accessor: 'products[0].dosage_form',
+        },
+        {
+          Header: 'Route',
+          accessor: 'products[0].route',
         },
         ],
      },
-    {
-      Header: 'Order Info',
-        columns: [
-        {
-          Header: 'Date',
-          accessor: 'date',
-        },
-       {
-          Header: 'Order #',
-          accessor: 'order',
-       },
-      ],
-    },
   ],
   []
  );
 
-
+ 
   return (
     <div className="App">
       <header className="App-header">
          <p>Drug discovery app</p>
          <Styles>
-          <Table columns={columns} data={data} />
+          <Table columns={columns} data={drug} />
          </Styles>
       </header>
     </div>
