09318e9a682e4594db4cdb7d1a3154dba7a23b3c
Arata Kagan <aratakagan@gmail.com>
2021-02-06 23:15:19 -0800

    Add Style.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,7 @@
-import React from 'react';
-import styled from 'styled-components'
+import React, {useState, useEffect} from 'react';
+import {Styles} from './Style.js';
 import './App.css';
 import Table from './components/Table';
-
-const Styles = styled.div`
-  padding: 1rem;
-
-  table {
-    border-spacing: 0;
-    border: 1px solid black;
-
-    tr {
-      :last-child {
-        td {
-          border-bottom: 0;
-        }
-      }
-    }
-
-    th,
-    td {
-      margin: 0;
-      padding: 0.5rem;
-      border-bottom: 1px solid black;
-      border-right: 1px solid black;
-
-      :last-child {
-        border-right: 0;
-      }
-    }
-  }
-`
 
 
 function App() {
